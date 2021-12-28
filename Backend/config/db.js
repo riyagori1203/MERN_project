d159e3b46ec2add4849a7d667cfb045e077adc90
riyagori1203 <66380988+riyagori1203@users.noreply.github.com>
2021-12-28 18:20:19 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// dbURI="mongodb+srv://riya:<Riya1203>@wevolve.xcvnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-module.export = mongoose.connect("mongodb+srv://riya:<Riya1203>@wevolve.xcvnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+module.export = mongoose.connect(process.env.dbURI)
   .then((result) =>console.log("db is connected"))
   .catch((err) => console.log(err));
